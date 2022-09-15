983daf75d3f22f7087035578f8cb5f3a6bd464b2
9914kach <9914kach@gmail.com>
2022-09-15 11:02:59 +0200

    Tests work properly

--- test/test_api.py
+++ test/test_api.py
@@ -10,6 +10,3 @@ class TestApi:
     def test_api_encoding(self):
         r = requests.get("http://18.202.253.30:8080")
         assert r.encoding == "utf-8", "Encoding is not utf-8"
-        
-        
-    
