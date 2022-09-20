429c4cac4a541387188beb43e7a21a82af71dc22
Jesper <peje20le@student.ju.se>
2022-09-20 09:58:02 +0200

    chargepoints table created in local database

--- test/test_postgresql.py
+++ test/test_postgresql.py
@@ -5,7 +5,7 @@ from postgres_connection import psql_connection
 
 
 def test_create_database():
-    connection = psql_connection.connect("postgres")
+    connection = psql_connection.connect("example_database")
     cursor = psql_connection.createCursor(connection)
 
     # Attempt to create a table
@@ -16,7 +16,7 @@ def test_create_database():
 
 def test_create_Table():
     # Establsih connection and create a cursor to execute commands in the database
-    connection = psql_connection.connect("testing_database7")
+    connection = psql_connection.connect("example_database")
     cursor = psql_connection.createCursor(connection)
 
     # Attempt to create a table
@@ -27,11 +27,12 @@ def test_create_Table():
 
 def test_insert_to_table():
     # Establsih connection and create a cursor to execute commands in the database
-    connection = psql_connection.connect("testing_database7")
+    connection = psql_connection.connect("example_database")
     cursor = psql_connection.createCursor(connection)
-    insertQuery = "INSERT INTO chargertable5 (chargername,created_on) VALUES ('chargerTestName',{timestamp}) RETURNING chargepointid".format(
-        timestamp=datetime.datetime.now(datetime.timezone.utcoffset(hour=1)))
-    dataToInsert = ("chargerTestName2")
+    insertQuery = '''INSERT INTO 
+        public."ChargePoints"(name, location, price, klarnaReservationAmount) 
+        VALUES("Nässjö Centralstation",{57.652328901782795,14.694810832097543},123.00,500);
+    '''
     cursor.execute(insertQuery)
     psql_connection.closeConnection(connection)
 
@@ -40,7 +41,7 @@ def createDataBase(cursor, connection):
     print('Attempt to create Database \n')
     try:
         # Create a table structure
-        cursor.execute('CREATE DATABASE testing_database7')
+        cursor.execute('CREATE DATABASE example_database')
     except:
         pytest.fail(
             'An error exception was raised! Is the Database already created?')
@@ -51,20 +52,22 @@ def createDataBase(cursor, connection):
 
 
 def createTables(cursor, connection):
-
+    chargePoints_Table = '''CREATE TABLE IF NOT EXISTS public."ChargePoints" (
+        ChargePointID SERIAL PRIMARY KEY NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        location DOUBLE PRECISION NOT NULL,
+        price NUMERIC(10,2) NOT NULL,
+        klarnaReservationAmount INTEGER NOT NULL
+    );'''
+    cursor.execute(chargePoints_Table)
     print('Attempt to create Tables \n')
     try:
         # Create a table structure
-        cursor.execute('''CREATE TABLE chargerTable5 (
-            chargePointID SERIAL PRIMARY KEY,
-            chargerName VARCHAR(20) UNIQUE NOT NULL,
-            created_on TIMESTAMP NOT NULL,
-            last_login TIMESTAMP
-            );''')
+        print('hi')
     except:
         pytest.fail(
             'An error exception was raised! Is the table already created?')
     else:
-        # if successful created, commit the table to the database
+        # if table was successfully created, commit the table to the database
         print('Table was successfully created!')
         connection.commit()
